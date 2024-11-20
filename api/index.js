36b966faa05be739f4ecccdd79c0322398e49792
DerickNoya <derick.noya@gmail.com>
2024-11-20 19:58:06 -0300

    port 3001

--- api/index.js
+++ api/index.js
@@ -58,7 +58,7 @@ app.post('/block/:assetId(\d+)', (req, res) => {
         });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
